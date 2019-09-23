5592812359f9227b3741ac7a7553fc5315f17a0c
xuzhaopeng <xuzhaopengsyl@163.com>
2019-09-23 16:40:35 +0800

    新增看点

--- autonomy-uni/pages/mine/mine.vue
+++ autonomy-uni/pages/mine/mine.vue
@@ -1,30 +1,28 @@
 <template>
 	<view>
 		<scroll-view :scroll-y="modalName==null" class="page" :class="modalName!=null?'show':''">
-			<view class="cu-bar bg-white solid-bottom margin-top">
-				<view class="action">
-					<text class="cuIcon-title text-orange "></text> 菜单
-				</view>
-			</view>
-			<view class="cu-modal" :class="modalName=='gridModal'?'show':''" @tap="hideModal">
-				<view class="cu-dialog" @tap.stop>
-					<radio-group class="block" @change="Gridchange">
-						<view class="cu-list menu text-left">
-							<view class="cu-item" v-for="(item,index) in 3" :key="index">
-								<label class="flex justify-between align-center flex-sub">
-									<view class="flex-sub">{{index +3}} 列</view>
-									<radio class="round" :value="(index + 3) + ''" :class="gridCol==index+3?'checked':''" :checked="gridCol==index+3"></radio>
-								</label>
+			<view class="padding text-center user">
+				<view class="padding-xl radius shadow-blur bg-red margin-top bg-img user-nav" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big91005.jpg);">
+					<view class="cu-avatar round lg" style="backgroundImage:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg)">
+						<view class="cu-tag badge" :class="sex==0?'cuIcon-female bg-pink':'cuIcon-male bg-blue'"></view>
+						<!-- :class="index%2==0?'cuIcon-female bg-pink':'cuIcon-male bg-blue'" -->
+					</view>
+					<view class="user-name">今晚打老虎</view>
+					<view class="user-note">
+						<view class="cu-capsule radius">
+							<view class='cu-tag bg-green sm'>
+								<text class='cuIcon-likefill'></text>
+							</view>
+							<view class="cu-tag line-green sm">
+								233
 							</view>
 						</view>
-					</radio-group>
-					<view class="cu-list menu text-left solid-top">
-						<view class="cu-item">
-							<view class="content">
-								<text class="text-grey">边框</text>
+						<view class="cu-capsule radius">
+							<view class='cu-tag bg-red sm'>
+								<text class='cuIcon-rechargefill'></text>
 							</view>
-							<view class="action">
-								<switch @change="Gridswitch" :class="gridBorder?'checked':''" :checked="gridBorder?true:false"></switch>
+							<view class="cu-tag line-red sm">
+								238
 							</view>
 						</view>
 					</view>
@@ -41,7 +39,50 @@
 				</view>
 			</view>
 
-
+			<!--  -->
+			<view class="cu-list menu" :class="[menuBorder?'sm-border':'',menuCard?'card-menu margin-top':'']">
+				<view class="cu-item" :class="menuArrow?'arrow':''">
+					<navigator class="content" hover-class="none" url="../list/list" open-type="redirect">
+						<text class="cuIcon-discoverfill text-orange"></text>
+						<text class="text-grey">Navigator 跳转</text>
+					</navigator>
+				</view>
+				<view class="cu-item" :class="menuArrow?'arrow':''">
+					<view class="content">
+						<text class="cuIcon-emojiflashfill text-pink"></text>
+						<text class="text-grey">头像组</text>
+					</view>
+					<view class="action">
+						<view class="cu-avatar-group">
+							<view class="cu-avatar round sm" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg);"></view>
+							<view class="cu-avatar round sm" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big81005.jpg);"></view>
+							<view class="cu-avatar round sm" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big25002.jpg);"></view>
+							<view class="cu-avatar round sm" style="background-image:url(https://ossweb-img.qq.com/images/lol/web201310/skin/big91012.jpg);"></view>
+						</view>
+						<text class="text-grey text-sm">4 人</text>
+					</view>
+				</view>
+				<view class="cu-item" :class="menuArrow?'arrow':''">
+					<view class="content">
+						<text class="cuIcon-tagfill text-red  margin-right-xs"></text>
+						<text class="text-grey">标签</text>
+					</view>
+					<view class="action">
+						<view class="cu-tag round bg-orange light">音乐</view>
+						<view class="cu-tag round bg-olive light">电影</view>
+						<view class="cu-tag round bg-blue light">旅行</view>
+					</view>
+				</view>
+				<view class="cu-item" :class="menuArrow?'arrow':''">
+					<view class="content">
+						<text class="cuIcon-warn text-green"></text>
+						<text class="text-grey">文本</text>
+					</view>
+					<view class="action">
+						<text class="text-grey text-sm">小目标还没有实现！</text>
+					</view>
+				</view>
+			</view>
 		</scroll-view>
 	</view>
 </template>
@@ -50,27 +91,13 @@
 	export default {
 		data() {
 			return {
+				sex: 0,
 				cuIconList: [{
-					cuIcon: 'cardboardfill',
-					color: 'red',
-					badge: 120,
-					name: 'VR'
-				}, {
-					cuIcon: 'recordfill',
-					color: 'orange',
-					badge: 1,
-					name: '录像'
-				}, {
-					cuIcon: 'picfill',
-					color: 'yellow',
-					badge: 0,
-					name: '图像'
-				}, {
 					cuIcon: 'noticefill',
 					color: 'olive',
 					badge: 22,
 					name: '通知'
-				}, {
+				},{
 					cuIcon: 'upstagefill',
 					color: 'cyan',
 					badge: 0,
@@ -80,11 +107,6 @@
 					color: 'blue',
 					badge: 0,
 					name: '皮肤'
-				}, {
-					cuIcon: 'discoverfill',
-					color: 'purple',
-					badge: 0,
-					name: '发现'
 				}, {
 					cuIcon: 'questionfill',
 					color: 'mauve',
@@ -102,14 +124,11 @@
 					name: '版权'
 				}],
 				modalName: null,
-				gridCol: 5,
+				gridCol: 4,
 				gridBorder: false,
 				menuBorder: false,
 				menuArrow: false,
 				menuCard: false,
-				skin: false,
-				listTouchStart: 0,
-				listTouchDirection: null,
 			};
 		},
 		methods: {
@@ -133,9 +152,6 @@
 			},
 			MenuCard(e) {
 				this.menuCard = e.detail.value
-			},
-			SwitchSex(e) {
-				this.skin = e.detail.value
 			},
 
 			// ListTouch触摸开始
@@ -185,5 +201,29 @@
 
 	.switch-music::before {
 		content: "\e6db";
+	}
+
+	/* 个人信息 */
+	.user {
+		padding-top: 2.5px;
+	}
+
+	.user-nav {
+		height: 200px;
+	}
+
+	.cu-avatar.lg {
+		width: 59px;
+		height: 59px;
+		font-size: 2em;
+		border: 2px solid #fff;
+	}
+
+	.user-name {
+		margin-top: 12.5px;
+	}
+
+	.user-note {
+		margin-top: 12.5px;
 	}
 </style>
